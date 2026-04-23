86522ef78ed04f1c6f8ed12a5d0feea172686702
dudiday <dudi199784@gmail.com>
2026-04-23 22:48:46 +0900

    fullstack 구조로 변경

---
+++ backend/src/main/java/com/slowind/chunchunhee/ChunchunheeApplication.java
@@ -0,0 +1,13 @@
+package com.slowind.chunchunhee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunchunheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunchunheeApplication.class, args);
+	}
+
+}
